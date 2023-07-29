6c95c9ba00d9aa8c0582b3119d0fc09b19920b86
Pratham Kashyap <prathamkashyap1980@gmail.com>
2023-07-29 17:07:15 +0530

    first commit

---
+++ FaceDetector.py
@@ -0,0 +1,25 @@
+import cv2
+
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, test_img = cap.read()  # captures frame and returns boolean value and captured image
+    if not ret:
+        continue
+    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+    for (x, y, w, h) in faces_detected:
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 255, 255), thickness=3)
+
+    resized_img = cv2.resize(test_img, (1000, 700))
+    cv2.imshow('Detected Faces', resized_img)
+
+    if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+        break
+
+cap.release()
+cv2.destroyAllWindows
